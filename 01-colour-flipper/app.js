9282433a037f94112fa5d610ea119c2d7caecef6
claudialsm <68153639+claudialsm@users.noreply.github.com>
2022-08-26 21:38:04 +0100

    Add js for colour flipper project, to change colour via user input

--- 01-colour-flipper/app.js
+++ 01-colour-flipper/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.input-form');
+const colourName = document.querySelector('.colour-name');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent the window from reloading
+  let colour = document.getElementById('colour').value.toLowerCase();
+  document.body.style.backgroundColor = colour;
+  colourName.innerHTML = colour;
+  colourName.style.color = colour;
+});
